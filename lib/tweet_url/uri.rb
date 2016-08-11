a1c91a73f8caa0b3ed3e4f4c12ba1a8bc8bcf12a
Kenta Yamamoto <ymkjp@users.noreply.github.com>
2016-08-12 00:32:20 +0900

    Now ready to release

--- lib/tweet_url/uri.rb
+++ lib/tweet_url/uri.rb
@@ -1,6 +1,10 @@
 module TweetUrl
   module Uri
 
+    # Retrieves REST resource ID
+    # @param list [String[]]
+    # @param &block [Proc]
+    # @return [Integer, NilClass]
     def self.find_resource_id(list, &block)
       index = list.index(&block)
       return index if index.nil?
@@ -8,6 +12,7 @@ module TweetUrl
       target.nil? ? target : target.to_i
     end
 
+    # To support old style URL like http://twitter.com/#!/status/759813164686938117
     # @param object [URI]
     # @return [String]
     def self.hashbang_path_or_path(object)
